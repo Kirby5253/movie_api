d93bbdb437c20a5074b6453255d3f55f17ce5d6b
Jacob Kirby <65231998+Kirby5253@users.noreply.github.com>
2020-06-09 19:30:55 -0500

    added 3 movies to database

--- index.js
+++ index.js
@@ -2,6 +2,33 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 
+let movies = [
+	{
+		name: 'Iron Man',
+		description:
+			'Dead men tell no tales Sea Legs nipperkin topgallant sheet wherry bring a spring upon her cable take a caulk black jack landlubber or just lubber. Nelsons folly Brethren of the Coast bilged on her anchor weigh anchor coffer interloper gibbet rutters piracy pressgang. Grog scurvy topsail gaff marooned sutler stern boom Jolly Roger rum. Brig loot cog fire in the hole strike colors boom grapple hearties Corsair bucko.',
+		genre: [ 'action', 'superhero', 'war', 'adventure', 'science fiction' ],
+		director: 'Jon Favreau',
+		image: '#'
+	},
+	{
+		name: 'Joker',
+		description:
+			'Dead men tell no tales Sea Legs nipperkin topgallant sheet wherry bring a spring upon her cable take a caulk black jack landlubber or just lubber. Nelsons folly Brethren of the Coast bilged on her anchor weigh anchor coffer interloper gibbet rutters piracy pressgang. Grog scurvy topsail gaff marooned sutler stern boom Jolly Roger rum. Brig loot cog fire in the hole strike colors boom grapple hearties Corsair bucko.',
+		genre: [ 'crime', 'drama', 'thriller' ],
+		director: 'Todd Phillips',
+		image: '#'
+	},
+	{
+		name: '1917',
+		description:
+			'Dead men tell no tales Sea Legs nipperkin topgallant sheet wherry bring a spring upon her cable take a caulk black jack landlubber or just lubber. Nelsons folly Brethren of the Coast bilged on her anchor weigh anchor coffer interloper gibbet rutters piracy pressgang. Grog scurvy topsail gaff marooned sutler stern boom Jolly Roger rum. Brig loot cog fire in the hole strike colors boom grapple hearties Corsair bucko.',
+		genre: [ 'war', 'drama' ],
+		director: 'Sam Mendes',
+		image: '#'
+	}
+];
+
 app.use(morgan('common'));
 
 app.use(express.static('public'));
@@ -16,7 +43,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/movies', (req, res) => {
-	res.json(top10);
+	res.json(list_of_movies.json);
 });
 
 app.listen(5000, () => {
