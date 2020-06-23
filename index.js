1552b499d909504218a708f068f49140e519321a
Jacob Kirby <65231998+Kirby5253@users.noreply.github.com>
2020-06-23 17:54:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -185,8 +185,8 @@ app.post(
 	}
 );
 
-// Get all users
-/*app.get('/users', passport.authenticate('jwt', { session: false }), (req, res) => {
+
+app.get('/users', passport.authenticate('jwt', { session: false }), (req, res) => {
 	Users.find()
 		.then((users) => {
 			res.status(201).json(users);
@@ -195,7 +195,7 @@ app.post(
 			console.error(err);
 			res.status(500).send('Error: ' + err);
 		});
-});*/
+});
 
 // Allows details to be viewed of a user by username
 app.get('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
