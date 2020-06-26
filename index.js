3cb3a61e0f35295927a8ece23cf83a316dcf2859
Jacob Kirby <65231998+Kirby5253@users.noreply.github.com>
2020-06-26 10:35:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.use('/client', express.static(path.join(__dirname, 'client', 'dist')));
 
 app.use((err, req, res, next) => {
 	console.error(err.stack);
-	res.status(500).send('Something broke!', JSON.stringify(err));
+	res.status(500).send('Something broke!', JSON.stringify(err.stack));
 });
 
 // Welcome page for API
